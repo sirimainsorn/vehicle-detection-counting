05c2b2938ea2fd6e89d3c6b8d2cecad5dc9217bc
Sirima Insorn <sirimainsorn@gmail.com>
2022-04-20 17:15:58 +0700

    [feat] read and write image

---
+++ ReadWriteImage.py
@@ -0,0 +1,7 @@
+import cv2
+
+img_grayscale = cv2.imread('test.jpg',0)
+cv2.imshow('graycsale image',img_grayscale)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+cv2.imwrite('grayscale.jpg',img_grayscale)
